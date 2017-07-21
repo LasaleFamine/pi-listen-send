d87299c65113d3d158d5b7909b13127defe69273
LasaleFamine <info@godev.space>
2017-07-21 22:40:28 +0200

    fixed query string sms

--- src/send_sms.py
+++ src/send_sms.py
@@ -1,12 +1,15 @@
 import urllib2
+import urllib
 
 def send(config):
 	api_url = config['sms']['api_url']
 	recipient = config['sms']['to']
 	message = config['sms']['message']
+
+	query_string = urllib.urlencode({ 'recipient' : recipient, 'message' : message})
 	
 	try:
-		res = urllib2.urlopen(api_url + '&recipient=' + recipient + '&message=' + message).read()
+		res = urllib2.urlopen(api_url + '&' query_string).read()
 	except: 
 		return False
 	finally:
